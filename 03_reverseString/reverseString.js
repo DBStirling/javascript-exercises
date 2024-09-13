114236f3e62c67cab714553a11e46ace6551406b
DBStirling <davidstirling17@gmail.com>
2024-09-13 18:08:27 -0400

    q1-q4 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,21 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    let length = input.length;
+    let characters = [];
+    let character;
 
-};
+    for (let i = 0; i < length; i++){
+        character = input.slice(i, i+1);
+        characters[i] = character;
+    };
+
+    let output = '';
+
+    for (let i = length; i > 0; i--) {
+        output += characters[i-1];
+    };   
+    
+    return output;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+    let size = arr.length;
+    let newArr = []
+
+    arr.forEach((item) => {
+
+        if (!nums.includes(item)) {
+            newArr.push(item);
+
+            }
+        });
+
+    return newArr;
 };
 
 // Do not edit below this line
