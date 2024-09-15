0477e5f9389c197ad68c64a1844d24eb0bdbae8a
DBStirling <davidstirling17@gmail.com>
2024-09-15 19:34:53 -0400

    q6 done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,8 @@
-const leapYears = function() {
+const leapYears = function(num) {
+
+return (num % 4 != 0) ? false :
+        (num % 100 != 0) ? true :
+                (num % 400 == 0) ? true : false;
 
 };
 
