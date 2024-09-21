d83cd850a4aa2141394e5dca452577ffe9c1c581
DBStirling <davidstirling17@gmail.com>
2024-09-21 18:16:32 -0400

    q9 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let cleanString = cleanUp(string);
+
+    len = cleanString.length;
+    halfLen = len/2;
+    isPalindrome = false;
+
+    for (let i = 0; i < halfLen; i++) {
+        if (cleanString[i] != cleanString[(len-1)-i]) {
+            console.log(i);
+            return false;
+        }
+    }
+    isPalindrome = true;
+    return true;
 
 };
+
+function cleanUp(string) {
+    newString = string.replace(/[!.',\s]/g,"");
+    loweredString = newString.toLowerCase();
+    return loweredString;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
